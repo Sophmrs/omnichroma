7dc8656bbacfd33f16f1179f9ec53fdfb371ac92
Theo Menkes Reis <theomreis@gmail.com>
2017-07-31 23:55:58 -0300

    Added more options, tooltips, fixed color repetition due to overflow

--- colors.js
+++ colors.js
@@ -3,8 +3,10 @@
 
 const widthInput = document.querySelector('[data-js="width"]');
 const heightInput = document.querySelector('[data-js="height"]');
-const colorMethod = document.querySelector('[data-js="method"]');
+const avgRange = document.querySelector('[data-js="avgRange"]');
 const paintersQty = document.querySelector('[data-js="painters"]');
+const selectionMode = document.querySelector('[data-js="selectionMode"]');
+const colorComparison = document.querySelector('[data-js="colorComparison"]');
 const seedMode = document.querySelector('[data-js="seedMode"]');
 const seedQty = document.querySelector('[data-js="seedQty"]');
 const seedColor = document.querySelector('[data-js="seedColor"]');
@@ -21,10 +23,11 @@ const ctx = canvas.getContext('2d');
 
 let paintingNum = 0;
 
-let colorCount;
-let colorMult;
+let colorDepth;
+let colorSkip;
 
 let colors = [];
+let labColors = [];
 let anchors = [];
 
 let width;
@@ -39,9 +42,9 @@ document.body.addEventListener('keypress', KbStart);
 
 class Color {
   constructor(r, g, b) {
-    this.r = r;
-    this.g = g;
-    this.b = b;
+    this.r = Math.round(r);
+    this.g = Math.round(g);
+    this.b = Math.round(b);
   }
 
   toStyle() {
@@ -50,6 +53,66 @@ class Color {
 
   distanceSqrFrom(color) {
     return (color.r - this.r) ** 2 + (color.g - this.g) ** 2 + (color.b - this.b) ** 2;
+  }
+
+  //Based on http://www.easyrgb.com/en/math.php
+  //Goes from RGB to XYZ to LAB
+  toColorLAB(){
+    const rgb = [this.r, this.g, this.b].map((c) => {
+      let color = c / 255;
+      color = color > 0.04045 ? ((color + 0.055) / 1.055) ** 2.4 : color / 12.92;
+      return color * 100;
+    });
+    const [xyzR,xyzG,xyzB] = rgb;
+    const x = (xyzR * 0.4124 + xyzG * 0.3576 + xyzB * 0.1805) * 0.95047;
+    const y = (xyzR * 0.2126 + xyzG * 0.7152 + xyzB * 0.0722) * 1;
+    const z = (xyzR * 0.0193 + xyzG * 0.1192 + xyzB * 0.9505) * 1.08883;
+ 
+    const labxyz = [x,y,z].map((c)=>{
+      let color = c/100;
+      return color > 0.008856 ? color ** (1/3) : (color * 7.787) + (16 /116);
+    });
+ 
+    const [labX, labY, labZ] = labxyz;
+    const l = (labY * 116) - 16;
+    const a = (labX - labY) * 500;
+    const b = (labY - labZ) * 200;
+ 
+    return new ColorLAB(l,a,b);
+  }
+}
+
+class ColorLAB{
+  constructor(l,a,b){
+    this.l = l;
+    this.a = a;
+    this.b = b;
+  }
+ 
+  // Based on https://github.com/THEjoezack/ColorMine/blob/master/ColorMine/ColorSpaces/Comparisons/Cie94Comparison.cs
+  deltaEFrom(labColor){
+    const k1 = .045;
+    const k2 = .015;
+ 
+    const dL = this.l - labColor.l;
+    const dA = this.a - labColor.a;
+    const dB = this.b - labColor.b;
+ 
+    const c1 = Math.sqrt(this.a ** 2 + this.b ** 2);
+    const c2 = Math.sqrt(labColor.a ** 2 + labColor.b ** 2);
+    const dC = c1 - c2;
+ 
+    const dH = Math.sqrt(Math.max(dA ** 2 + dB ** 2 - dC ** 2, 0));
+ 
+    const sC = 1 + k1 * c1;
+    const sH = 1 + k2 * c1;
+ 
+    const l = dL;
+    const c = dC / sC;
+    const h = dH / sH;
+ 
+    const dESqr = l ** 2 + c ** 2 + h ** 2;
+    return dESqr < 0 ? 0 : Math.sqrt(dESqr);
   }
 }
 
@@ -96,7 +159,6 @@ class Anchor {
     return availablePositions;
   }
 
-  //Almost same as above, but returns early
   hasAvailableNeighbours() {
     const minX = Math.max(0, this.pos.x - 1);
     const maxX = Math.min(ctx.canvas.width - 1, this.pos.x + 1);
@@ -108,23 +170,13 @@ class Anchor {
 
     const imgData = ctx.getImageData(minX, minY, width, height);
 
-    let curPos = new Pos(minX, minY);
-
     for (let i = 3; i < width * height * 4; i += 4) {
       if (imgData.data[i] === 0)
         return true;
-
-      const newPos = new Pos(curPos.x, curPos.y);
-      newPos.x++;
-      if (newPos.x > maxX) {
-        newPos.x = minX;
-        newPos.y++;
-      }
-      curPos = newPos;
     }
     return false;
   }
-
+  
 }
 
 class Painter {
@@ -144,28 +196,36 @@ class Painter {
           this.isPainting = false;
           return;
         }
-        const idx = anchors.indexOf(PickRandom(anchors));
+
+        let idx;
+        if(selectionMode.value === 'random')
+          idx = anchors.indexOf(PickRandom(anchors));
+        else if(selectionMode.value === 'worm')
+          idx = anchors.length - 1;
+        else if(selectionMode.value === 'root')
+          idx = 0;
+
         anchor = anchors.splice(idx, 1)[0];
         canUseAnchor = anchor.hasAvailableNeighbours();
       } while (!canUseAnchor);
 
       const availablePos = anchor.getAvailableNeighbours();
       const pos = PickRandom(availablePos);
-      const color = PickClosestColor(anchor.color);
-
-      ctx.fillStyle = color.toStyle();
-      ctx.fillRect(pos.x, pos.y, 1, 1);
+      const color =  PickClosestColor(anchor.color);
 
       const newAnchor = new Anchor(pos, color);
       if (newAnchor.hasAvailableNeighbours()) {
         anchors.push(newAnchor);
       }
 
+      ctx.fillStyle = color.toStyle();
+      ctx.fillRect(pos.x, pos.y, 1, 1);
+
       //Add anchor back if it still has available neighbours
       if (anchor.hasAvailableNeighbours()) {
         anchors.push(anchor);
       }
-      if (this.isPainting)
+      if (this.isPainting && colors.length > 0)
         this.animationFrame = requestAnimationFrame(() => this.Draw());
     } else {
       console.count(`Painter finished drawing painting ${paintingNum}!`);
@@ -193,9 +253,10 @@ function Init() {
   canvas.width = width;
   canvas.height = height;
 
-  //Number of colors per channel
-  colorCount = 2 ** ((Math.log2(width) + Math.log2(height)) / 3);
-  colorMult = Math.max(Math.ceil(256 / colorCount), 1);
+  //Bits of colors per channel
+  colorDepth = 2 ** ((Math.log2(width) + Math.log2(height)) / 3);
+  //Used to fill the color array with the necessary colors
+  colorSkip = 256 / colorDepth;
 
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 
@@ -215,13 +276,15 @@ function Start() {
   paintingNum++;
 
   colors = [];
+  labColors = [];
   anchors = [];
 
   //Set all colors
-  for (let i = 0; i < colorCount; i++) {
-    for (let j = 0; j < colorCount; j++) {
-      for (let k = 0; k < colorCount; k++) {
-        colors.push(new Color(colorMult * i, colorMult * j, colorMult * k));
+  for (let i = 0; i < colorDepth; i++) {
+    for (let j = 0; j < colorDepth; j++) {
+      for (let k = 0; k < colorDepth; k++) {
+        colors.push(new Color(colorSkip * i, colorSkip * j, colorSkip * k));
+        labColors.push(colors[colors.length-1].toColorLAB());
       }
     }
   }
@@ -312,25 +375,26 @@ function KbStart(e) {
 function PickClosestColor(color) {
   let minDist = Infinity;
   let curColor = null;
+  let labColor;
+
+  const labCompare = (colorComparison.value === 'lab');
+  if(labCompare)
+     labColor = color.toColorLAB();
   for (let i = 0; i < colors.length; i++) {
-    const dist = color.distanceSqrFrom(colors[i]);
+    let dist;
+    if(labCompare) {
+      dist = labColor.deltaEFrom(labColors[i]);
+    }
+    else{
+      dist = color.distanceSqrFrom(colors[i]);
+    }
     if (dist < minDist) {
       minDist = dist;
       curColor = i;
     }
   }
+  labColors.splice(curColor, 1);
   return colors.splice(curColor, 1)[0];
-}
-
-function IndexOfColor(color) {
-  for (let i = 0; i < colors.length; i++) {
-    if (colors[i].r === color.r &&
-      colors[i].g === color.g &&
-      colors[i].b === color.b) {
-      return i;
-    }
-  }
-  return null;
 }
 
 function PickRandom(arr) {
